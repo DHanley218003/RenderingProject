8be6ebf445e1bcf4f6e5c513a8c4f5bdc1a25b3b
Declan Hanley <a00218003@student.ait.ie>
2017-09-28 16:24:02 +0100

    Moved cause gpu

--- objloadertest/aitmesh.cpp
+++ objloadertest/aitmesh.cpp
@@ -6,44 +6,37 @@
 #include <iostream>
 #include <assert.h>
 
-const int BUFFSIZE = 80;
+const unsigned int BUFFSIZE = 80; //each line should be less than 80 characters
 
 bool aitMesh::loadFromObj(std::string path)
 {
-	/*aitVertex v1(glm::vec3(0.000000, 1.000000, 0.000000), glm::vec3(0.000000, 0.000000, 1.000000));
-	aitVertex v2(glm::vec3(-1.000000, -1.000000, 0.000000), glm::vec3(0.000000, 0.000000, 1.000000));
-	aitVertex v3(glm::vec3(1.000000, -1.000000, 0.000000), glm::vec3(0.000000, 0.000000, 1.000000));
-	
-	vertices.push_back(v1);
-	vertices.push_back(v2);
-	vertices.push_back(v3);*/
+	// temporary vectors to store data until it's joined into the final vector
 	std::vector<glm::vec3> tempVertices;
 	std::vector<glm::vec3> tempNormals;
 	std::string temp;
 	char buff[BUFFSIZE];
-	float tempx, tempy, tempz;
-	unsigned int x, y;
+	float tempx, tempy, tempz; // used to create vertex and vertex normals
+	unsigned int x, y; // used to pair up vertex and vertex normals
 
 	std::ifstream infile(path);
 	std::stringstream ss;
-	std::stringstream ss2;
 
 	while(true)
 	{
-		if(infile.eof())
+		if(infile.eof()) // exits loop when it reaches the end of the file
 			break;
 		infile.getline( buff, BUFFSIZE);
 		ss.clear();
-		ss.str("");
+		ss.str(""); // clears and sets the string stream buffer to empty
 		ss << buff;
-		if(buff[0]=='o' || buff[0] == '#')
-		{}
-		else if(buff[0]=='v')
+		if(buff[0]=='o' || buff[0] == '#') // if comment or object name, ignore
+		{} // example input: o Cube
+		else if(buff[0]=='v') // if vector
 		{
-			if(buff[1]=='n')
+			if(buff[1]=='n') // if vector normal
 			{
-				ss >> temp >> tempx >> tempy >> tempz;
-				tempNormals.push_back(glm::vec3(tempx, tempy, tempz));
+				ss >> temp >> tempx >> tempy >> tempz; // example input: v -1.000000 -1.000000 1.000000
+				tempNormals.push_back(glm::vec3(tempx, tempy, tempz)); 
 			}
 			else
 			{
@@ -52,27 +45,28 @@ bool aitMesh::loadFromObj(std::string path)
 				tempVertices.push_back(glm::vec3(tempx, tempy, tempz));
 			}
 		}
-		else if(buff[0]=='s')
-		{}
-		else if(buff[0]=='f')
+		else if(buff[0]=='s') // TODO
+		{} // example input: s off
+		else if(buff[0]=='f') // if faces
 		{
-			bool flipflop = true;
-			while (ss >> temp)
+			bool flipflop = true; // faces come in x//y, so flipflop alternates which is loaded
+			while (ss >> temp) // example input: f 1//1 2//1 3//1
 			{
-				if (temp != "" && temp != "/" && temp != "//")
+				if (temp != "" && temp != "/" && temp != "//") // makes sure only numbers are in it
 				{
 					if (flipflop)
 					{
-						x = atoi(temp.c_str());
+						x = atoi(temp.c_str()); // must be converted to c string to use atoi
 						flipflop = !flipflop;
 					}
 					else
 					{
 						y = atoi(temp.c_str());
 						flipflop = !flipflop;
+						vertices.push_back(aitVertex(tempVertices[x], tempNormals[y])); // join the vertex and normal, then send to vertices
 					}
 				}
-				vertices.push_back(aitVertex(tempVertices[x], tempNormals[y]));
+				
 			}
 		}
 	}
